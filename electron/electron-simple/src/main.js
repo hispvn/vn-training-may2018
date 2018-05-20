cdcb7f8152d8681bb97e9b108b0881ef1283f3c8
Morten Olav Hansen <mortenoh@gmail.com>
2018-05-20 09:30:36 +0700

    introduce husky/prettier

---
+++ electron/electron-simple/src/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  )
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
